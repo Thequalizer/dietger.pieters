b0e720f0ada3fa3eb7f747d1b15fe2b252b5efc7
Thequalizer <p.dietger@hotmail.com>
2018-01-02 13:19:54 +0100

    polyline added for route to race destination

--- app/src/main/java/dietgerpieters/werkstuk/Fragments/MapsTabFragment.java
+++ app/src/main/java/dietgerpieters/werkstuk/Fragments/MapsTabFragment.java
@@ -4,14 +4,19 @@ import android.Manifest;
 import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,20 +29,32 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.maps.DirectionsApi;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.GeocodingApiRequest;
+import com.google.maps.android.PolyUtil;
 import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsResult;
 import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.TravelMode;
+
+import org.joda.time.DateTime;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 import dietgerpieters.werkstuk.Database.AppDatabase;
 import dietgerpieters.werkstuk.Models.Wedstrijd;
 import dietgerpieters.werkstuk.R;
 
+import static android.content.ContentValues.TAG;
 import static android.content.Context.LOCATION_SERVICE;
 
 /**
@@ -195,8 +212,59 @@ public class MapsTabFragment extends Fragment implements OnMapReadyCallback {
     private final LocationListener mLocationListener = new LocationListener() {
         @Override
         public void onLocationChanged(final Location location) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
             latLngCurrent = new LatLng(location.getLatitude(), location.getLongitude());
             mMap.addMarker(new MarkerOptions().position(latLngCurrent).title("Current position"));
+
+
+
+
+            DateTime now = new DateTime();
+            try {
+
+
+
+
+
+
+                double lat = location.getLatitude();
+                double lon = location.getLongitude();
+                int maxResults = 1;
+
+                Geocoder gc = new Geocoder(getContext(), Locale.getDefault());
+                List<Address> addresses = gc.getFromLocation(lat, lon, maxResults);
+
+
+                // Handle case where no address was found.
+                if (addresses == null || addresses.size()  == 0) {
+
+                } else {
+                    Address address = addresses.get(0);
+                    ArrayList<String> addressFragments = new ArrayList<String>();
+
+                    // Fetch the address lines using getAddressLine,
+                    // join them, and send them to the thread.
+                    for(int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                        addressFragments.add(address.getAddressLine(i));
+                    }
+
+                    DirectionsResult result = DirectionsApi.newRequest(getGeoContext()).mode(TravelMode.DRIVING).origin(TextUtils.join(System.getProperty("line.separator"),
+                            addressFragments)).destination("Beitem").departureTime(now).await();
+
+                    addPolyline(result,mMap);
+                }
+
+
+
+
+            } catch (ApiException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
         }
 
@@ -216,5 +284,16 @@ public class MapsTabFragment extends Fragment implements OnMapReadyCallback {
         }
     };
 
+    private GeoApiContext getGeoContext() {
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey("AIzaSyCJOitdqPwgXCgIPw0__SRt64lfKFHp_xw")
+                .build();
+
+        return context;
+    }
+    private void addPolyline(DirectionsResult results, GoogleMap mMap) {
+        List<LatLng> decodedPath = PolyUtil.decode(results.routes[0].overviewPolyline.getEncodedPath());
+        mMap.addPolyline(new PolylineOptions().addAll(decodedPath));
+    }
 }
 
